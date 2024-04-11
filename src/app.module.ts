50d16b7c75dea2b7a684c9bc01174da071c7e217
drako9159 <chochito9159@gmail.com>
2024-04-11 00:34:35 -0600

    feat(tasks): define configurations tasks

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { TasksModule } from './tasks/tasks.module';
 import { ProjectsModule } from './projects/projects.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { HelloController } from './hello/hello.controller';
 
 @Module({
   imports: [TasksModule, ProjectsModule, AuthModule, UsersModule],
-  controllers: [AppController],
+  controllers: [HelloController],
   providers: [AppService],
 })
 export class AppModule {}
